1298b216a6b9c7638589f180e437b6cfc475d63d
wuxuan <1422541058@qq.com>
2021-04-24 20:43:59 +0800

    添加了一个Student类

---
+++ src/main/java/cn/hxzy/controller/Student.java
@@ -0,0 +1,12 @@
+package cn.hxzy.controller;
+
+public class Student {
+    private String name;
+    private int age;
+
+    public String print(String name,int age){
+       return name + age;
+    }
+
+
+}
